a83e485f0dd70608423c598bc42cf177acabc7ce
ronesanttos <ronesanttos17@gmailcom>
2026-05-18 22:10:42 -0300

    atualizando backend

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -174,16 +174,13 @@ LOGGING = {
         'level': os.environ.get('DJANGO_LOG_LEVEL', 'INFO'),
     },
 }
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
-
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
